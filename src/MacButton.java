6bc4659e568b4ca72595ce8e58bac91e1a0b59bf
vladimir <vlj2007@yandex.ru>
2025-01-01 02:48:05 +0700

    I added the class MacCheckbox.java

---
+++ src/MacButton.java
@@ -0,0 +1,6 @@
+// Конкретные продукты для второй семьи
+public class MacButton implements Button{
+    public void paint() {
+        System.out.println("MacButton: paint");
+    }
+}
---
+++ src/MacCheckbox.java
@@ -0,0 +1,5 @@
+public class MacCheckbox implements Checkbox{
+    public void paint() {
+        System.out.println("MacCheckbox: paint");
+    }
+}
